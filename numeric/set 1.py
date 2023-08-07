5680978a46803a0cc9f157ea358ac2359954a164
lavizah <lavizahpanlekar76@gmail.com>
2023-08-07 10:01:17 +0530

    set

---
+++ numeric/set 1.py
@@ -0,0 +1,11 @@
+# set
+s1={1,2,3,4,5,6}
+print('s1=',s1,len(s1))
+s2={1,2,2,3,3,3,4,5,6}
+print('s2=',s2,len(s2))
+s3={'lavizah','ashfiya','aaisha'}
+print('s3=',s3,len(s3))
+s4={'lavizah','ashfiya','aaisha',1,1,2,3,4,6,8,9}
+print('s4=',s4,len(s4))
+s5={}
+print('s5=',s5,len(s5))
